4a44ca14b7ce66f55a085de297463a73d5977a31
Lorenz aka Lolo Hänggi <lolo8304@gmail.com>
2023-05-16 08:40:15 +0000

    implement step 3 but still with ./ only

--- grep.js
+++ grep.js
@@ -10,24 +10,29 @@ const readline = require('readline');
 
 program.version('1.0.0');
 
-program
-  .arguments('<pattern> [files...]')
-  .option('-r')
-  .description('grep from code challenge')
-  .action((pattern, files, options, commands) => {
-    var promise = grep(pattern, files, options, readStreamFromFilesOrConsole(files, options));
-    promise
-    .then((result) => {
-    if (result && result.length > 0) {
+
+async function actionMethod (pattern, files, options, commands) {
+    var totalCount = await grep(pattern, files, options, readStreamFromFilesOrConsole(files, options));
+    if (totalCount > 0) {
       process.exit(0);
     } else {
       process.exit(1);
     }
-  })
-    
-  });
+  }
+
+program
+  .arguments('<pattern> [files...]')
+  .option('-r')
+  .description('grep from code challenge')
+  .action(actionMethod);
 
 program.parse(process.argv);
+
+function wildcardToRegExp(wildcard) {
+  const escapedWildcard = wildcard.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  const regex = escapedWildcard.replace(/\*/g, '.*').replace(/\?/g, '.');
+  return new RegExp(`^${regex}$`);
+}
 
 
 function iterateFiles(dir, filePatterns, options, callback) {
@@ -36,19 +41,20 @@ function iterateFiles(dir, filePatterns, options, callback) {
   files.forEach((file) => {
     const filePath = path.join(dir, file);
     const isDirectory = fs.statSync(filePath).isDirectory();
+    //console.log(filePath);
 
     if (options.r && isDirectory) {
       // Recursively iterate over subfolders
       iterateFiles(filePath, filePatterns, options, callback);
     } else {
+      //console.log("--------")
       // Check if the file matches any of the specified patterns
       const matchesPattern = filePatterns.some((pattern) => {
-        const regex = new RegExp(pattern);
+        const regex = new RegExp(wildcardToRegExp(pattern));
         return regex.test(file);
       });
 
       if (matchesPattern) {
-        // Perform operations on the matched file
         callback(filePath);
       }
     }
@@ -60,14 +66,18 @@ function readStreamFromFilesOrConsole(files, options) {
   if (files && files.length > 0) {
     var filteredFiles =[]
    
-   iterateFiles("", filePatterns, options, (file) => {
+   iterateFiles("./", files, options, (file) => {
     filteredFiles.push(file)
    });
 
-   return filteredFiles.map((file) => {
-     name: file,
-     stream: fs.createReadStream(file)
-   })
+   const filteredStreams = filteredFiles.map((file) => {
+     const fsStream = fs.createReadStream(file);
+     return {
+       name: file,
+       stream: fsStream
+     }
+   });
+   return filteredStreams;
   } else {
     const stream = new Readable({
       read(size) {
@@ -86,6 +96,7 @@ function readStreamFromFilesOrConsole(files, options) {
 
 
 function readLinesFromStream(streamObj, callback) {
+  //console.log("read from "+streamObj.name)
   const rl = readline.createInterface({
     input: streamObj.stream,
     crlfDelay: Infinity
@@ -112,20 +123,26 @@ function readLinesFromStream(streamObj, callback) {
 
 
 
-function grep(pattern, files, options, streams){
+async function grep(pattern, files, options, streams) {
   var logName = files && files.length > 1
   
-  return readLinesFromStream(streams[0], (line) => {
-    const found = containsPattern(pattern, line);
-    if (found) {
-      if (logName) {
-        console.log(streamObj.name+":"+line)
-      } else {
-        console.log(line);
+  var totalFound=0;
+  for(const stream of streams) {
+    var mypromise = readLinesFromStream(stream, (line) => {
+      const found = containsPattern(pattern, line);
+      if (found) {
+        if (logName) {
+          console.log(stream.name+":"+line)
+        } else {
+          console.log(line);
+        }
       }
-    }
-     return found;
-})
+    })
+    
+    var resultList = await mypromise;
+    totalFound += resultList.length
+  }
+  return totalFound
 }
 
 function containsPattern(pattern, line) {
